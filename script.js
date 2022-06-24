b46400fee8d6fe64a0faed5af37199968ba62d86
djtravz <djtravz@gmail.com>
2022-06-24 17:17:59 -0400

    minor changes + mobile versions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function toggleTheme() {
     document.getElementById("mode").classList.toggle("dark");
 }
+
+function getAspect() {
+    console.log((window.innerWidth / window.innerHeight),window.innerWidth , window.innerHeight);
+    window.innerWidth
+}
